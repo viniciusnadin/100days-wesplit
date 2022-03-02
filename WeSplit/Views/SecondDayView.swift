3c8d09bf1e3048648994878b9cf6091381462cac
Vinicius Nadin <vininadin@gmail.com>
2022-03-02 00:32:37 -0300

    creating pickers in a form

--- WeSplit/Views/SecondDayView.swift
+++ WeSplit/Views/SecondDayView.swift
@@ -11,24 +11,33 @@ struct SecondDayView: View {
     
     // @State because we want to read and rwite var values from struct
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     
     private let tipPercentages = [0, 10, 15, 20, 25]
     
     var body: some View {
-        Form {
-            Section {
-                // $ simple to make the variable a 2 way binding
-                // Locale.current.currencyCode gets the local currency of the device
-                // .keyboardType is a modifier 
-                TextField("Amount", value: self.$checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                    .keyboardType(.decimalPad)
-            }
-            
-            Section {
-                Text(self.checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-            }
+        NavigationView {
+            Form {
+                Section {
+                    // $ simple to make the variable a 2 way binding
+                    // Locale.current.currencyCode gets the local currency of the device
+                    // .keyboardType is a modifier
+                    TextField("Amount", value: self.$checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+                
+                Picker("Number of people", selection: self.$numberOfPeople) {
+                    ForEach(1 ..< 100) {
+                        Text("\($0)")
+                    }
+                }
+                
+                Section {
+                    Text(self.checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+                // We put the navigationTitle in the form because navigation view are capable of showing many views as your program runs.
+            }.navigationTitle("WeSplit")
         }
     }
 }
